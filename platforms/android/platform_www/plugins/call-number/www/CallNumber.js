c3d9817e5eecc2bb4ccc9a93a699ab4813891ecf
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2019-02-05 12:44:41 +0530

    `

--- platforms/android/platform_www/plugins/call-number/www/CallNumber.js
+++ platforms/android/platform_www/plugins/call-number/www/CallNumber.js
@@ -1,8 +1,13 @@
+cordova.define("call-number.CallNumber", function(require, exports, module) {
 var CallNumber = function(){};
 
 CallNumber.prototype.callNumber = function(success, failure, number, bypassAppChooser){
     cordova.exec(success, failure, "CallNumber", "callNumber", [number, bypassAppChooser]);
 };
+
+CallNumber.prototype.isCallSupported = function(success, failure){
+    cordova.exec(success, failure, "CallNumber", "isCallSupported");
+}
 
 //Plug in to Cordova
 cordova.addConstructor(function() {
@@ -13,4 +18,6 @@ cordova.addConstructor(function() {
 
     if(!window.plugins) window.plugins = {};
     window.plugins.CallNumber = new CallNumber();
+});
+
 });

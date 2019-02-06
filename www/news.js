7df90730c4e299cb16052865a82feb4cade49900
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2019-02-06 11:39:36 +0530

    `

--- www/news.js
+++ www/news.js
@@ -1,32 +1,4 @@
 $(document).ready(function(){
-    $(".main-news").empty();
-            $(function() {
-        		$.ajax({
-            		type: "get",
-            		url: "https://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms" ,
-            		crossDomain: true,
-            		dataType: "xml",
-            		success: function(data) {
-                		console.log(data);
-                		var $xml = $(data);
-                		$xml.find("item").each(function() {
-                    		var $this = $(this),
-                        	item = {
-                            	title: $this.find("title").text(),
-                            	link: $this.find("link").text(),
-                            	description: $this.find("description").text(),
-                            	pubDate: $this.find("published").text(),
-                            	author: $this.find("author").text()
-                        	};
-                    	console.log(item);
-                    	$(".toi-news").append('<div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator"></div><div class="card-content "><span class="card-title activator grey-text text-darken-4" style="font-size:300%; line-height:110%;">'+item.title+'</span><p><a href="'+item.link+'" class="btn green">Click to view whole article</a></p></div>');
-                	});
-            	},
-            	error: function(xhr, status) {
-					console.log(status);
-            	}
-        	});
-    		});
 	$('.dropdown-trigger').dropdown();
 	$("li").click(function(){
         if($(this).text()==="TOI"){

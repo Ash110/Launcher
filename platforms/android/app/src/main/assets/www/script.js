c80cc089d4326946f6856fa403d99c4651e96717
Ashwin <ashwin.alaparthi@gmail.com>
2019-02-07 21:53:11 +0530

    Stuff

--- platforms/android/app/src/main/assets/www/script.js
+++ platforms/android/app/src/main/assets/www/script.js
@@ -1,84 +1,84 @@
 function addcontact(){
-    $(".add-contact").remove();
-    $('#modal1').modal('open');
-    $(".add-cntct-btn").click(function(){
-        var user_name = document.getElementById("user_name").value;
-        var user_num = document.getElementById("user_num").value;
-        $(".quick-contacts").append("<div class='contacts' number='"+user_num+"'><img src='https://image.flaticon.com/icons/svg/145/145867.svg'><p>"+user_name+"</p></div>");
-        $(".quick-contacts").append("<div class='contacts add-contact' onclick='addcontact()'><img src='https://image.flaticon.com/icons/svg/78/78294.svg'><p>Create New</p></div>");
-    });
+	$(".add-contact").remove();
+	$('#modal1').modal('open');
+	$(".add-cntct-btn").click(function(){
+		var user_name = document.getElementById("user_name").value;
+		var user_num = document.getElementById("user_num").value;
+		$(".quick-contacts").append("<div class='contacts' number='"+user_num+"'><img src='https://image.flaticon.com/icons/svg/145/145867.svg'><p>"+user_name+"</p></div>");
+		$(".quick-contacts").append("<div class='contacts add-contact' onclick='addcontact()'><img src='https://image.flaticon.com/icons/svg/78/78294.svg'><p>Create New</p></div>");
+	});
 }
 $(document).ready(function() {
 	var what_btn;
-    $('.example').swichTab({
-        index: 1
-    });
-    $('.tabs').tabs();
-    var d = new Date();
-    console.log(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
-    function formatAMPM(date) {
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var ampm = hours >= 12 ? 'PM' : "AM";
-        hours = hours % 12;
-        hours = hours ? hours : 12; // the hour '0' should be '12'
-        minutes = minutes < 10 ? '0'+minutes : minutes;
-        var strTime = hours + ':' + minutes ;
-        $(".time").text(strTime);
-    }
-    formatAMPM(d);
-    setInterval(function(){
-        formatAMPM(d);
-    },30000);
-    var week = {1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday",0:"Sunday"};
-    $(".day").text(week[d.getDay()]);
-    var months = {0:"January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June",
-                      6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December" };
-    $(".date").text(d.getDate()+" "+months[d.getMonth()]);
-    $(".contacts").click(function(){
-        var num = $(this).attr("number");
-        function onSuccess(result){
-            console.log("Success:"+result);
-        }
-        function onError(result) {
-            alert("Error:"+result);
-        }
-        window.plugins.CallNumber.callNumber(onSuccess, onError, num, true);
-    });
-    setInterval(function(){
+	$('.example').swichTab({
+		index: 1
+	});
+	var currentAudio=0;	
+	var d = new Date();
+	console.log(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+	function formatAMPM(date) {
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var ampm = hours >= 12 ? 'PM' : "AM";
+		hours = hours % 12;
+		hours = hours ? hours : 12; // the hour '0' should be '12'
+		minutes = minutes < 10 ? '0'+minutes : minutes;
+		var strTime = hours + ':' + minutes ;
+		$(".time").text(strTime);
+	}
+	formatAMPM(d);
+	setInterval(function(){
+		formatAMPM(d);
+	},30000);
+	var week = {1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday",0:"Sunday"};
+	$(".day").text(week[d.getDay()]);
+	var months = {0:"January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June",
+				  6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December" };
+	$(".date").text(d.getDate()+" "+months[d.getMonth()]);
+	$(".contacts").click(function(){
+		var num = $(this).attr("number");
+		function onSuccess(result){
+			console.log("Success:"+result);
+		}
+		function onError(result) {
+			alert("Error:"+result);
+		}
+		window.plugins.CallNumber.callNumber(onSuccess, onError, num, true);
+	});
+	setInterval(function(){
 		firebase.database().ref('Quick-Note').once('value').then(function(snapshot) {
-        	var note = (snapshot.val());
-        	console.log("You have a note from "+note.Sender+" . "+note.Note); 
-        	$(".quick-notes p").text("You have a note from "+note.Sender+" . "+note.Note);
-    	});
+			var note = (snapshot.val());
+			console.log("You have a note from "+note.Sender+" . "+note.Note); 
+			$(".quick-notes p").text("You have a note from "+note.Sender+" . "+note.Note);
+		});
 	},2000);
-    $(".listen-note").click(function(){
-        TTS.speak($(".quick-notes p").text()).then(function () {
-            alert('success');
-        }, function (reason) {
-            alert(reason);
-        });
-    });
-    $(".dialer").click(function(){
-        var sApp = startApp.set({
-            "application":"com.google.android.contacts"
-        });
-        sApp.start(function() { /* success */
-            console.log("OK");
-        }, function(error) { /* fail */
-            alert(error);
-        });
-    });
-    $(".video-call").click(function(){
-        var sApp = startApp.set({
-            "application":"com.google.android.apps.tachyon"
-        });
-        sApp.start(function() { /* success */
-            console.log("OK");
-        }, function(error) { /* fail */
-            alert(error);
-        });
-    });
+	$(".listen-note").click(function(){
+		TTS.speak($(".quick-notes p").text()).then(function () {
+			alert('success');
+		}, function (reason) {
+			alert(reason);
+		});
+	});
+	$(".dialer").click(function(){
+		var sApp = startApp.set({
+			"application":"com.google.android.contacts"
+		});
+		sApp.start(function() { /* success */
+			console.log("OK");
+		}, function(error) { /* fail */
+			alert(error);
+		});
+	});
+	$(".video-call").click(function(){
+		var sApp = startApp.set({
+			"application":"com.google.android.apps.tachyon"
+		});
+		sApp.start(function() { /* success */
+			console.log("OK");
+		}, function(error) { /* fail */
+			alert(error);
+		});
+	});
 	var gn = new GyroNorm();
 	var args = {
 		frequency:50,					// ( How often the object sends the values - milliseconds )
@@ -89,15 +89,15 @@ $(document).ready(function() {
 		screenAdjusted:false			// ( If set to true it will return screen adjusted values. )
 	};
 	gn.init(args).then(function(){
-    	gn.start(function(data){
-        	var aa = Math.pow((Math.pow(data.dm.gx,2) + Math.pow(data.dm.gy,2)+Math.pow(data.dm.gz,2)),(1/2));
-        	if(aa>50){
+		gn.start(function(data){
+			var aa = Math.pow((Math.pow(data.dm.gx,2) + Math.pow(data.dm.gy,2)+Math.pow(data.dm.gz,2)),(1/2));
+			if(aa>50){
 				$('#modal-fall').modal('open');
 				currentAudio = document.createElement('audio');
-        		var fileName=String("annoying-alarm-tone.mp3");
-        		currentAudio.setAttribute('src', fileName);
-        		currentAudio.setAttribute('autoplay', 'autoplay');
-        		currentAudio.play();
+				var fileName=String("annoying-alarm-tone.mp3");
+				currentAudio.setAttribute('src', fileName);
+				currentAudio.setAttribute('autoplay', 'autoplay');
+				currentAudio.play();
 				var t=5;
 				setInterval(function(){
 					if(t>0){
@@ -106,32 +106,34 @@ $(document).ready(function() {
 					}
 				},1000);
 				setTimeout(function(){
-        			function onSuccess(result){
-            			console.log("Success:"+result);
-        			}
-        			function onError(result) {
-            			alert("Error:"+result);
-        			}
-        			window.plugins.CallNumber.callNumber(onSuccess, onError, "9999999999", true);
+					function onSuccess(result){
+						console.log("Success:"+result);
+					}
+					function onError(result) {
+						alert("Error:"+result);
+					}
+					window.plugins.CallNumber.callNumber(onSuccess, onError, "9999999999", true);
 				},6000);
-        	}
-  		});
+			}
+		});
 	}).catch(function(e){
-  		alert("Error");
+		alert("Error");
 	});
 	$('.modal').modal();
 	$(".cancel-fall").click(function(){
 		$('#modal-fall').modal('close');
+		if(currentAudio!==0)
+            currentAudio.pause();
 	});
 	$(".contacts").click(function(){
-        function onSuccess(result){
-            console.log("Success:"+result);
-        }
-        function onError(result) {
-            alert("Error:"+result);
-        }
-        window.plugins.CallNumber.callNumber(onSuccess, onError, "9999999999", true); 
-    });
+		function onSuccess(result){
+			console.log("Success:"+result);
+		}
+		function onError(result) {
+			alert("Error:"+result);
+		}
+		window.plugins.CallNumber.callNumber(onSuccess, onError, "9999999999", true); 
+	});
 	$(".whats-cnt").click(function(){
 		$("#whatsapp-modal").modal("open");
 		what_btn = $(this).text();
@@ -147,6 +149,6 @@ $(document).ready(function() {
 			var what_mes = $(".whatsapp-message").val();
 			window.open("https://api.whatsapp.com/send?phone=+919449810576&text="+what_mes);	
 		}
-		else{alert(what_btn)}
+		else{alert(what_btn);}
 	});
 });
--- www/news.js
+++ www/news.js
@@ -1,65 +1,96 @@
 $(document).ready(function () {
+	$(".main-news").empty();
+	$(function() {
+		$.ajax({
+			type: "get",
+			url: "https://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms",
+			crossDomain: true,
+			dataType: "xml",
+			success: function(data) {
+				console.log(data);
+				var $xml = $(data);
+				$xml.find("item").each(function() {
+					var $this = $(this),
+						item = {
+							title: $this.find("title").text(),
+							link: $this.find("link").text(),
+							description: $this.find("description").text(),
+							pubDate: $this.find("published").text(),
+							author: $this.find("author").text()
+						};
+					console.log(item);
+					var imgLink = item.description.match("photo(.*).cms","g");
+					$(".main-news").append('<div class="card"><div class="card-image waves-effect waves-block waves-light"><img src="https://timesofindia.indiatimes.com/photo/'+imgLink[1]+'.cms"></div><div class="card-content "><span class="card-title activator grey-text text-darken-4" style="font-size:300%; line-height:110%;">'+item.title+'</span><p><a href="'+item.link+'" class="btn green">Click to view whole article</a></p></div>');
+				});
+			},
+			error: function(xhr, status) {
+				console.log(status);
+			}
+		});
+	});
 	$('.dropdown-trigger').dropdown();
 	$("li").click(function(){ 
-        if($(this).text()==="TOI"){
-            $(".main-news").empty();
-            $(function() {
-        		$.ajax({
+		if($(this).text()==="TOI"){
+			$(".header p").text("Times of India");
+			$(".main-news").empty();
+			$(function() {
+				$.ajax({
 					type: "get",
-            		url: "https://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms" ,
-            		crossDomain: true,
-            		dataType: "xml",
-            		success: function(data) {
-                		console.log(data);
-                		var $xml = $(data);
-                		$xml.find("item").each(function() {
-                    		var $this = $(this),
-                        	item = {
-                            	title: $this.find("title").text(),
-                            	link: $this.find("link").text(),
-                            	description: $this.find("description").text(),
-                            	pubDate: $this.find("published").text(),
-                            	author: $this.find("author").text()
-                        	};
-                    	console.log(item);
-                    	$(".toi-news").append('<div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator"></div><div class="card-content "><span class="card-title activator grey-text text-darken-4" style="font-size:300%; line-height:110%;">'+item.title+'</span><p><a href="'+item.link+'" class="btn green">Click to view whole article</a></p></div>');
-                	});
-            	},
-            	error: function(xhr, status) {
-					console.log(status);
-            	}
-        	});
-    		});
-        }
-        else if($(this).text()==="VK"){
-            $(".main-news").empty();
-         		$(function() {
-        		$.ajax({
-            		type: "get",	
-            		url: "https://vijaykarnataka.indiatimes.com/rssfeedstopstories.cms" ,		
-            		crossDomain: true,
-            		dataType: "xml",
-            		success: function(data) {
-                		console.log(data);
-                		var $xml = $(data);
-                		$xml.find("item").each(function() {
-                    		var $this = $(this),
-                        	item = {
-								title: $this.find("title").text(),
-                            	link: $this.find("link").text(),
-                            	description: $this.find("description").text(),
-                            	pubDate: $this.find("published").text(),
-                            	author: $this.find("author").text()
-                        	};
-                    	console.log(item);
-                    	$(".main-news").append('<div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator"></div><div class="card-content "><span class="card-title activator grey-text text-darken-4" style="font-size:300%; line-height:110%;">'+item.title+'</span><p><a href="'+item.link+'" class="btn green">Click to view whole article</a></p></div>');
-                	});
-            	},
-            	error: function(xhr, status) {
-					console.log(status);
-            	}
-        	});
-    	});   
-     }
+					url: "https://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms" ,
+					crossDomain: true,
+					dataType: "xml",
+					success: function(data) {
+						console.log(data);
+						var $xml = $(data);
+						$xml.find("item").each(function() {
+							var $this = $(this),
+								item = {
+									title: $this.find("title").text(),
+									link: $this.find("link").text(),
+									description: $this.find("description").text(),
+									pubDate: $this.find("published").text(),
+									author: $this.find("author").text()
+								};
+							console.log(item);
+							$(".main-news").append('<div class="card"><div class="card-image waves-effect waves-block waves-light"><img src="https://timesofindia.indiatimes.com/photo/'+imgLink[1]+'.cms"></div><div class="card-content "><span class="card-title activator grey-text text-darken-4" style="font-size:300%; line-height:110%;">'+item.title+'</span><p><a href="'+item.link+'" class="btn green">Click to view whole article</a></p></div>');
+						});
+					},
+					error: function(xhr, status) {
+						console.log(status);
+					}
+				});
+			});
+		}
+		else if($(this).text()==="VK"){
+			$(".header p").text("Vijay Karnataka");
+			$(".main-news").empty();
+			$(function() {
+				$.ajax({
+					type: "get",	
+					url: "https://vijaykarnataka.indiatimes.com/rssfeedstopstories.cms" ,		
+					crossDomain: true,
+					dataType: "xml",
+					success: function(data) {
+						console.log(data);
+						var $xml = $(data);
+						$xml.find("item").each(function() {
+							var $this = $(this),
+								item = {
+									title: $this.find("title").text(),
+									link: $this.find("link").text(),
+									description: $this.find("description").text(),
+									image: $this.find("image").text(),
+									author: $this.find("author").text()
+								};
+							console.log(item);
+							$(".main-news").append('<div class="card"><div class="card-image waves-effect waves-block waves-light"><img src="'+item.image+'"></div><div class="card-content "><span class="card-title activator grey-text text-darken-4" style="font-size:300%; line-height:110%;">'+item.title+'</span><p><a href="'+item.link+'" class="btn green">Click to view whole article</a></p></div>');
+						});
+					},
+					error: function(xhr, status) {
+						console.log(status);
+					}
+				});
+			});   
+		}
 	});
 });

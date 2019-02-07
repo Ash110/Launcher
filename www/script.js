982057da7dd61e39064a63fef5c262012681e719
Ashwin <ashwin.alaparthi@gmail.com>
2019-02-07 23:11:51 +0530

    `

--- www/script.js
+++ www/script.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
 	}
 	formatAMPM(d);
 	setInterval(function(){
+		var d = new Date();
 		formatAMPM(d);
 	},30000);
 	var week = {1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday",0:"Sunday"};
